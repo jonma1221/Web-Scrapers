883ff4f40ed7eb2fb5dfcdf6ff78a3dd6b350c71
Jon <jonma1221@gmail.com>
2026-08-14 13:55:22 -0700

    Added gh action for deploying gh pages

--- groceries/webapp/ui/vite.config.ts
+++ groceries/webapp/ui/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/Web-Scrapers/",
   server: {
     proxy: {
       // "/api": "https://web-scrapers-docker.onrender.com/",
